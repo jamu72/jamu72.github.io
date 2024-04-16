74bdc95d56aa039b24f12fecd33eeb7d691825a5
jamu72 <82548643+jamu72@users.noreply.github.com>
2024-04-15 21:46:25 -0600

    Sadge WA 12 finished

---
+++ wa/wa12/ica12.js
@@ -0,0 +1,44 @@
+const endpoint = 'https://dog.ceo/api/breeds/image/random';
+const btn = document.querySelector("#js-new-quote");
+const answerBtn = document.querySelector('#js-tweet');
+const picture = document.querySelector('#picture')
+answerBtn.addEventListener("click", getAnswer);
+btn.addEventListener("click", getPicture);
+
+let answer = '';
+
+
+async function getPicture(){
+    console.log("Button to get new quote has been clicked");
+    try{
+        const response = await fetch(endpoint);
+        // if(!response.ok){
+        //     throw Error(response.statusText);
+        // }
+        const json = await response.json();
+        console.log(json);
+        imageSrc = json['message'];
+        console.log(imageSrc);
+        picture.src = imageSrc;
+        // displayQuote(json['question']);
+        // console.log(json['answer']);
+        // answer = json['answer'];
+
+    }catch(err){
+        console.log(err)
+        alert("Failed to fetch the new quote");
+    }
+    console.log("get quote");
+}
+
+function displayQuote(quote){
+    console.log("Hello in quote");
+    const quoteText = document.querySelector("#js-quote-text");
+    quoteText.textContent = quote;
+}
+
+function getAnswer(){
+    answerTxt.textContent = answer;
+    console.log("Hello in answer");
+}
+
